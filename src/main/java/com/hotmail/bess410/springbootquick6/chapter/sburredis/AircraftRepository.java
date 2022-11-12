df175c5e551be99d70fed2d7f297e0a927428e74
Andrey Sterkhov <asterkhov@65apps.com>
2022-11-12 13:32:26 +0400

    6 chapter using Repository

---
+++ src/main/java/com/hotmail/bess410/springbootquick6/chapter/sburredis/AircraftRepository.java
@@ -0,0 +1,6 @@
+package com.hotmail.bess410.springbootquick6.chapter.sburredis;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AircraftRepository extends CrudRepository<Aircraft, Long> {
+}
